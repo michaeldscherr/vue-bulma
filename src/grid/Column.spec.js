3bd0d349c7b90d36f2aae6c7bd97244760053a3e
Michael Scherr <mds@planitagency.com>
2017-03-15 10:35:01 -0400

    adds new Column test

--- src/grid/Column.spec.js
+++ src/grid/Column.spec.js
@@ -7,12 +7,30 @@ function getRenderedText (Component, propsData) {
     return vm.$el.textContent;
 }
 
+function getViewModel (Component, propsData) {
+    const Ctor = Vue.extend(Component);
+    const vm = new Ctor({ propsData }).$mount();
+    return vm;
+}
+
 (function ColumnSpec(describe, it, expect) {
-    describe('Column', () => {
+    window.beforeEach(() => {
+      window.spyOn(console, 'error');
+    });
+    describe('Column w/ Correct Size', () => {
         it('renders correctly', () => {
-            expect(getRenderedText(Column, {
+            getViewModel(Column, {
                 size: 2,
-            }));
+            });
+            expect(console.error).not.toHaveBeenCalled();
+        });
+    });
+    describe('Column w/ Incorrect Size', () => {
+        it('renders correctly', () => {
+            getViewModel(Column, {
+                size: 13,
+            });
+            expect(console.error).toHaveBeenCalled();
         });
     });
 }(window.describe, window.it, window.expect));
